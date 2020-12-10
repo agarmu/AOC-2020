1fd8e730c12a01dc46ed08f1cdaf6103966cac33
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2020-12-10 17:20:35 -0600

    linting

--- src/day8.rs
+++ src/day8.rs
@@ -12,10 +12,10 @@ impl Instruction {
             "acc" => Self::ACC(parts.next().unwrap().parse().unwrap()),
             "jmp" => Self::JMP(parts.next().unwrap().parse().unwrap()),
             "nop" => Self::NOP(parts.next().unwrap().parse().unwrap()),
-            _ => panic!("{}", input)
+            _ => panic!("{}", input),
         }
     }
-    pub fn mutate_nop_jmp(&self)  -> Self {
+    pub fn mutate_nop_jmp(&self) -> Self {
         match self {
             Self::NOP(i) => Self::JMP(*i),
             Self::JMP(i) => Self::NOP(*i),
@@ -25,21 +25,22 @@ impl Instruction {
 }
 pub type InstructionVisited = (Instruction, bool);
 
-
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct VM {
-    pub instructions: Vec<InstructionVisited>, 
-    pub acc: i32, 
-    pub ptr: usize
+    pub instructions: Vec<InstructionVisited>,
+    pub acc: i32,
+    pub ptr: usize,
 }
-
 
 impl VM {
     pub fn parse(input: String) -> Self {
         Self {
-            instructions: input.lines().map(|line| (Instruction::parse(line.to_owned()), false)).collect(),
-            acc: 0, 
-            ptr: 0
+            instructions: input
+                .lines()
+                .map(|line| (Instruction::parse(line.to_owned()), false))
+                .collect(),
+            acc: 0,
+            ptr: 0,
         }
     }
     pub fn halt_at_loop(&mut self) -> i32 {
@@ -52,12 +53,12 @@ impl VM {
             let instruction = &mut self.instructions[self.ptr];
             match instruction {
                 (_, true) => return self.acc,
-                (NOP(_), _) => self.ptr+= 1,
+                (NOP(_), _) => self.ptr += 1,
                 (ACC(amt), _) => {
                     self.acc += *amt;
                     self.ptr += 1;
-                },
-                (JMP(amt), _) => self.ptr = (self.ptr as i32 + *amt) as usize
+                }
+                (JMP(amt), _) => self.ptr = (self.ptr as i32 + *amt) as usize,
             }
             instruction.1 = true;
         }
@@ -65,7 +66,7 @@ impl VM {
     pub fn ends_properly(&mut self) -> bool {
         let len = self.instructions.len();
         loop {
-            if self.ptr == (len-1) {
+            if self.ptr == (len - 1) {
                 println!("Ends Properly: ACC = {}", self.acc);
                 return true;
             }
@@ -73,15 +74,15 @@ impl VM {
             let instruction = &mut self.instructions[self.ptr];
             match instruction {
                 (_, true) => {
-                   // println!("Terminated at {}: {:?}\tACC = {}", self.ptr, inst, self.acc);
+                    // println!("Terminated at {}: {:?}\tACC = {}", self.ptr, inst, self.acc);
                     return false;
                 }
-                (NOP(_), _) => self.ptr+= 1,
+                (NOP(_), _) => self.ptr += 1,
                 (ACC(amt), _) => {
                     self.acc += *amt;
                     self.ptr += 1;
-                },
-                (JMP(amt), _) => self.ptr = (self.ptr as i32 + *amt) as usize
+                }
+                (JMP(amt), _) => self.ptr = (self.ptr as i32 + *amt) as usize,
             }
             instruction.1 = true;
         }
@@ -107,14 +108,19 @@ pub fn solve_part1(inp: &String) -> i32 {
 #[aoc(day8, part2)]
 pub fn solve_part2(inp: &String) -> i32 {
     let vm = VM::parse(inp.clone());
-    let flipping_targets: Vec<_> = vm.instructions.iter().enumerate().filter(|(_,(instruction,_))| {
-        if let Instruction::ACC(_) = instruction {
-            false
-        } else {
-            true
-        }
-    }).collect();
-    let flipping_target_indices: Vec<_> = flipping_targets.iter().map(|(i,_)| i).collect();
+    let flipping_targets: Vec<_> = vm
+        .instructions
+        .iter()
+        .enumerate()
+        .filter(|(_, (instruction, _))| {
+            if let Instruction::ACC(_) = instruction {
+                false
+            } else {
+                true
+            }
+        })
+        .collect();
+    let flipping_target_indices: Vec<_> = flipping_targets.iter().map(|(i, _)| i).collect();
     println!("{}", flipping_target_indices.len());
     for i in flipping_target_indices {
         //flip nop-jmp
@@ -123,7 +129,7 @@ pub fn solve_part2(inp: &String) -> i32 {
         vm.instructions[*i].0 = vm.instructions[*i].0.mutate_nop_jmp();
         println!("{:?} ", vm.instructions[*i].0);
         if vm.ends_properly() {
-            return vm.acc
+            return vm.acc;
         }
     }
     panic!("None?")
